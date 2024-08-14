1be711069fe004502860acfb7ab7f3e879990ee6
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-13 19:11:36 -0700

    list all documents in mongo collection python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+"""
+Python code that lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    List all documents in a collection
+    Args:
+        mongo_collection: the collection object
+    Return: object
+    """
+    return mongo_collection.find()
